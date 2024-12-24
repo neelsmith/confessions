fa2e3baa0b3fefb84162856f1308f8cb703df226
Neel Smith <dnsmith.neel@gmail.com>
2024-12-24 12:42:28 -0500

    Add some basic files

--- scripts/vocab.jl
+++ scripts/vocab.jl
@@ -22,3 +22,30 @@ distincttokens = length(counts)
 
 @info("Total number of lexical tokens: $(totallex)")
 @info("Distinct tokens: $(distincttokens)")
+
+
+using CitableParserBuilder, Tabulae
+
+tabulaeurl = "http://shot.holycross.edu/tabulae/complut-lat25-current.cex"
+parser = stringParser(tabulaeurl, UrlReader)
+
+
+vocab = keys(counts) |> collect
+
+parsing = true
+results = []
+
+
+for (i,tkn) in enumerate(vocab)
+    if parsing
+        parsed = parsetoken(tkn, parser)
+        if isempty(parsed)
+            parsing = false
+            @info("Stopped at index $(i), token $(tkn)")
+        else
+            push!(results, parsed)
+        end
+    end
+end
+
+parsetoken("et", parser)
