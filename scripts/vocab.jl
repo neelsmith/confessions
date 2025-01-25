b4a72d6fa9e37af824916bf32ace6ea53016d262
Neel Smith <dnsmith.neel@gmail.com>
2025-01-25 07:35:55 -0500

    Analyze vocab

--- scripts/vocab.jl
+++ scripts/vocab.jl
@@ -1,22 +1,21 @@
 using CitableBase, CitableText, CitableCorpus
 using Downloads, Markdown
 
+
+localparser = true
+
 srcfile = joinpath(pwd(), "src", "confessions.cex")
-
 corpus = fromcex(srcfile, CitableTextCorpus, FileReader)
-
 
 using Orthography, LatinOrthography
 
 tkns = tokenize(corpus, latin24())
-
 lex = filter(t -> t.tokentype isa LexicalToken, tkns)
 totallex = length(lex)
 
 using StatsBase, OrderedCollections
 
 countsraw = map(tkn -> tkn.passage.text, lex) |> countmap |> OrderedDict
-
 counts = sort(countsraw, byvalue = true, rev = true)
 distincttokens = length(counts)
 
@@ -27,11 +26,21 @@ distincttokens = length(counts)
 
 using CitableParserBuilder, Tabulae
 
-tabulaeurl = "http://shot.holycross.edu/morphology/lewisshort-lat24-current.cex"
-parser = stringParser(tabulaeurl, UrlReader)
+tabulaerepo = joinpath(dirname(pwd()), "Tabulae.jl")
 
+function getparser(localparser::Bool = localparser; tabulae = tabulaerepo)
+    if localparser
+        parserfile = joinpath(tabulae, "scratch", "lewisshort-lat24-current.cex")
+        stringParser(parserfile, FileReader)
+    else
+        tabulaeurl = "http://shot.holycross.edu/morphology/lewisshort-lat24-current.cex"
+        stringParser(tabulaeurl, UrlReader)
+    end
+end
 
+parser = getparser()
 
+parsetoken("et", parser)
 
 nonsingletons = filter(counts) do (k,v)
     v > 1
@@ -40,9 +49,13 @@ repeatvocab = keys(nonsingletons) |> collect
 @info("Number of repeated tokens: $(length(repeatvocab))")
 
 
-# This takes < 5 secc on my laptop at home
+
 @time parses = map(repeatvocab) do wrd
     (token = wrd, parselist = parsetoken(wrd, parser))
+end
+
+successes = filter(parses) do tpl
+    ! isempty(tpl.parselist)
 end
 
 fails = filter(parses) do tpl
@@ -62,7 +75,71 @@ end
 
 pwd()
 
+function lexids(plist::Vector{NamedTuple{(:token, :parselist)}})
+    ustrings = plist.parselist .|> lexemeurn .|> string
+    ids = [replace(s, r"[^.]+\." => "") for s in ustrings]
+    unique(ids)
+end
 
+
+idvals = successes .|> lexids |> Iterators.flatten |> collect |> unique
+
+lexcounts = []
+
+#=
+Given lexeme:
+find all analyzed tokens
+find count for token
+=#
+function tokensforlemma(id::AbstractString, parses::Vector{NamedTuple{(:token, :parselist)}})
+    #@info("Work with $(id) and a $(typeof(parses))")
+
+    parsesforid = filter(parses) do p
+        #@info("Now get lexids for a $(p)")
+        allids = lexids(p)
+        id in allids
+    end
+    map(a -> a.token, parsesforid)
+end
+
+function lexcount(id::AbstractString, parses::Vector{NamedTuple{(:token, :parselist)}}, occurrences::OrderedDict{SubString{String}, Int64})
+    @info("Find count for $(id)")
+    tknsforid = tokensforlemma(id, parses)
+    [occurrences[tkn] for tkn in tknsforid] |> sum
+    #tknsforid
+end
+
+
+lexcountsall = OrderedDict()
+map(idvals[1:10]) do lsid
+    readable = labellemm(lsid)
+    numoccurrences = lexcount(lsid, successes, counts) 
+    lexcountsall[readable] = numoccurrences
+end
+
+sort!(lexcountsall; rev=true, byvalue = true)
+#@time lexfreqs = map(a -> lexcount(a.token, successes, counts), successes[1:100])
+
+
+dicoid = "n13803"
+lexcount(dicoid, successes, counts)
+
+
+lexcounts = countmap(idvals) |> OrderedDict
+sort!(lexcounts; rev=true, byvalue=true)
+
+lexlabels = Tabulae.lexlemma_dict_remote()
+function labellemm(id::AbstractString, dict = lexlabels)
+    keystring = string("ls.", id)
+    if haskey(dict, (keystring))
+        string(id, " (", dict[keystring], ")")
+    else
+        keystring * " (?)"
+    end
+end
+
+idlabels = [labellemm(s) for s in keys(lexcounts)]
+keys(lexcounts) |> collect
 #=
 parsing = true
 results = []
